764e4bcd47ccb1342a95f8fd9ac94f07d08a2917
habibundayishimiye <habibundayishimiye@gmail.com>
2024-10-10 20:03:57 +0200

    change

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <Navbar />
+        <Main />
+    </>
+  )
+}
+
+export default App
